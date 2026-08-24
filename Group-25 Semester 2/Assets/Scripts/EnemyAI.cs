54103ddce52bbfd1aef7443eede3d6fa8bef4d50
Leeroy-afk <marindaniseleeroy@gmail.com>
2026-08-24 19:23:07 +0200

    Revert "Light System and sanity added"
    This reverts commit b48e2cd516c2a9325cc4a0e796a32a259be464c9.

--- Group-25 Semester 2/Assets/Scripts/EnemyAI.cs
+++ Group-25 Semester 2/Assets/Scripts/EnemyAI.cs
@@ -5,25 +5,14 @@ public class EnemyAI : MonoBehaviour
 {
     public Transform player;
 
-    [Header("Normal Behaviour")]
+    [Header("Distance")]
     public float stopDistance = 5f;
 
-    [Header("Sanity Behaviour")]
-    public float attackDistance = 2f;
-    public float attackCooldown = 3f;
-
     private NavMeshAgent agent;
-    private PlayerSanity playerSanity;
-    private PlayerHealth playerHealth;
-
-    private float attackTimer;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
-        playerSanity = player.GetComponent<PlayerSanity>();
-        playerHealth = player.GetComponent<PlayerHealth>();
 
         agent.stoppingDistance = stopDistance;
     }
@@ -33,24 +22,7 @@ public class EnemyAI : MonoBehaviour
         if (player == null)
             return;
 
-        if (playerSanity.CurrentSanity <= 0)
-        {
-            AggressiveBehaviour();
-        }
-        else
-        {
-            NormalBehaviour();
-        }
-    }
-
-    void NormalBehaviour()
-    {
-        float distance = Vector3.Distance(
-            transform.position,
-            player.position
-        );
-
-        agent.stoppingDistance = stopDistance;
+        float distance = Vector3.Distance(transform.position, player.position);
 
         if (distance > stopDistance)
         {
@@ -62,39 +34,4 @@ public class EnemyAI : MonoBehaviour
             agent.isStopped = true;
         }
     }
-
-    void AggressiveBehaviour()
-    {
-        float distance = Vector3.Distance(
-            transform.position,
-            player.position
-        );
-
-        agent.stoppingDistance = attackDistance;
-
-        if (distance > attackDistance)
-        {
-            agent.isStopped = false;
-            agent.SetDestination(player.position);
-        }
-        else
-        {
-            agent.isStopped = true;
-
-            Attack();
-        }
-    }
-
-    void Attack()
-    {
-        attackTimer -= Time.deltaTime;
-
-        if (attackTimer <= 0f)
-        {
-            playerHealth.TakeHalfHealth();
-
-            attackTimer = attackCooldown;
-        }
-    }
-    
 }
